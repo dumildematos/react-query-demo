32b3ca2754f840a4ffb92deaec56c0d8014efd71
Dumilde Matos <dumildejosematos@gmail.com>
2022-03-11 23:39:29 +0100

    add ReactQueryDevtools

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { QueryClientProvider , QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
@@ -33,6 +34,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    <ReactQueryDevtools initialisOpen={false} position='bottom-right' />
   </QueryClientProvider>
 
   );
