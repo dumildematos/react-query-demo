162d85a84ad99f83e09bdb7268655cb5318e71c9
Dumilde Matos <dumildejosematos@gmail.com>
2022-03-11 21:50:24 +0100

    get from api using axios

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -2,7 +2,29 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 export const SuperHeroesPage = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheros').then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    })
+  }, []);
+
+  if(isLoading){
+    return <h2> Loading </h2>
+  }
+
   return (
-    <div>Superheros.page</div>
+    <>
+      <h2>Super Heros</h2>
+      {
+        data.map(hero => {
+          return <div key={hero.id}> {hero.name} </div>
+        })
+      }
+    </>
   )
 }
